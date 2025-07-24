9bdda3e7ea139f445ed09305e96324b09674e7e3
Alex Shtein <125296103+Alex-Shtein@users.noreply.github.com>
2025-07-24 13:26:03 +0300

    Delete build directory

--- build/CMakeFiles/mp_HW02Lib.dir/Users/artem/Documents/База_знаний_2.0/МП/mp_HW02/Task2/src/main.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/mp_HW02Lib.dir/Users/artem/Documents/База_знаний_2.0/МП/mp_HW02/Task2/src/main.cpp.o: \
-  /Users/artem/Documents/База\ знаний\ 2.0/МП/mp_HW02/Task2/src/main.cpp
--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/artem/Documents/База знаний 2.0/МП/mp_HW02/build/_deps/googletest-src/googletest
-# Build directory: /Users/artem/Documents/База знаний 2.0/МП/mp_HW02/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
